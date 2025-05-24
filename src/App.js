e9d55a2f8771b06d0c7113af5e06c1694149f2e2
Meksak Chilingaryan <mexakchilingaryan3@gmail.com>
2025-05-25 02:50:51 +0400

    feat: add welcome message in homepage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Բարի օր Էդգար ջան!</h1>
       <h1>Welcome to My Coin App 🚀</h1>
       <p>This is your React project integrated with Telegram!</p>
     </div>
