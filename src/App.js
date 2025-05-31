ba686a6e5c8e75b93338e16f240435b60bc944df
Meksak Chilingaryan <mexakchilingaryan3@gmail.com>
2025-05-31 15:48:44 +0400

    add clear console

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [user, setUser] = useState(null);
-alert(JSON.stringify(Object.keys(window), null, 2));
 
   useEffect(() => {
     const tg = window.Telegram?.WebApp;
